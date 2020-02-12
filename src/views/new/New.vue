aebac8d43037a9f53eaab1eae6c98ca432cc286f
sjc <songjunchao.cn@gmail.com>
2020-02-13 04:40:08 +0800

    页面添加分页

--- src/views/new/New.vue
+++ src/views/new/New.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="new">
     <List :listData = 'listData'/>
+    <Paging v-show="this.listData.length>0" :page-index="currentPage" :total="count" :page-size="pageSize" @change="pageChange" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import List from '@/components/list/List.vue'
+import Paging from '@/components/paging/Paging.vue'
 
 export default {
   name: 'New',
   components: {
-    List
+    List,
+    Paging
   },
   data(){
     return{
-      listData:[]
+      listData:[],
+      pageSize: 10, //每页显示20条数据
+      currentPage: 1, //当前页码
+      count: 1, //总记录数
     }
   },
   created(){
@@ -24,10 +30,17 @@ export default {
   mounted(){
   },
   methods:{
-    async getList(){
-      let params = {query:"{stories(type: new, first: 10) {id title time url by score descendants}}"}
+   async getList () {
+      let args = { query: `{stories(type: new) {id title time url by score descendants}}` }
+      let res = await this.$api.getListApi(args)
+      this.count = res.data.data.stories.length
+      let params = { query: `{stories(type: new, first: 10, skip: ${this.currentPage} ) {id title time url by score descendants}}` }
       let { data } = await this.$api.getListApi(params)
       this.listData = data.data.stories
+    },
+    pageChange (page) {
+      this.currentPage = page
+      this.getList()
     }
   }
 }
--- src/views/top/Top.vue
+++ src/views/top/Top.vue
@@ -1,33 +1,46 @@
 <template>
   <div class="top">
-    <List :listData = 'listData'/>
+    <List :listData="listData" />
+    <Paging v-show="this.listData.length>0" :page-index="currentPage" :total="count" :page-size="pageSize" @change="pageChange" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import List from '@/components/list/List.vue'
+import Paging from '@/components/paging/Paging.vue'
 
 export default {
   name: 'Top',
   components: {
-    List
+    List,
+    Paging
   },
-  data(){
-    return{
-      listData:[]
+  data () {
+    return {
+      listData: [],
+      pageSize: 10, //每页显示20条数据
+      currentPage: 1, //当前页码
+      count: 1, //总记录数
     }
   },
-  created(){
+  created () {
     this.getList()
   },
-  mounted(){
+  mounted () {
   },
-  methods:{
-   async getList(){
-      let params = {query:"{stories(type: top) {id title time url by score descendants}}"}
+  methods: {
+    async getList () {
+      let args = { query: `{stories(type: top) {id title time url by score descendants}}` }
+      let res = await this.$api.getListApi(args)
+      this.count = res.data.data.stories.length
+      let params = { query: `{stories(type: top, first: 10, skip: ${this.currentPage} ) {id title time url by score descendants}}` }
       let { data } = await this.$api.getListApi(params)
       this.listData = data.data.stories
+    },
+    pageChange (page) {
+      this.currentPage = page
+      this.getList()
     }
   }
 }

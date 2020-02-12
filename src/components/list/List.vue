c4f5458f2f4586f5066d63cbac256ed7a6b78140
sjc <songjunchao.cn@gmail.com>
2020-02-12 21:43:47 +0800

    list组件完成

---
+++ src/components/list/List.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <article class="wrap" v-for="item in $attrs.listData" :key="item.id">
+      <div class="flex-content">
+        <div class="left">
+          <div class="score-img">
+            <img :src="$imgUrls.images.up" alt />
+          </div>
+          <div class="score">{{item.score}}</div>
+        </div>
+        <div class="middle">
+          <div class="title">{{item.title}}</div>
+          <div class="url">{{item.url}}</div>
+          <div class="comment">
+            <!-- <div class="comment-wrap"> -->
+              <div>
+                <img :src="$imgUrls.images.user" alt />
+                <span class="user">44</span>
+              </div>
+              <div>
+                <img :src="$imgUrls.images.msg" alt />
+                <span class="msg">22</span>
+              </div>
+            <!-- </div> -->
+          </div>
+        </div>
+        <div class="right">
+          {{$dateFormat(item.time,'yyyy:MM:dd  hh:mm:ss') }}
+        </div>
+      </div>
+      <div class="line"></div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {
+    };
+  },
+  mounted () {
+    console.log(this.$attrs)
+  },
+  methods: {},
+};
+</script>
+
+<style lang='scss' scoped>
+@import "./list.scss";
+</style>

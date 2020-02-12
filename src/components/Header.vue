4d9615556bbf160f428080e195caf349cdbf0548
sjc <songjunchao.cn@gmail.com>
2020-02-12 11:20:45 +0800

    模块化图片路径

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,8 @@
 <template>
   <header>
-    <div class="app-center">
-      <nav class="nav">
+    <nav class="nav">
         <span>
-          <img :src="$imgUrls.homeImg.logo" alt />
+          <img :src="$imgUrls.Images.logo" alt />
         </span>
         <a
           :class="{'nav-index': item.name === tabName}"
@@ -12,16 +11,6 @@
           @click="changeTab(item.herf)"
         >{{item.text}}</a>
       </nav>
-      <div class="times">
-        <div class="container">
-          <div class="sunny"></div>
-        </div>
-        <!-- <a target="_blank" href="https://github.com/songjunchao-cn">
-          <img :src="$imgUrls.homeImg.git" alt />
-        </a> -->
-        <time>{{formatTime}}</time>
-      </div>
-    </div>
   </header>
 </template>
 
@@ -31,37 +20,16 @@ export default {
   data () {
     return {
       nav: [
-        { indent: true, name: 'home', text: 'HOME', herf: '/home' },
-        { indent: false, name: 'blog', text: 'BLOG', herf: '/blog' },
-        { indent: false, name: 'work', text: 'WORK', herf: '/work' },
-        { indent: false, name: 'about', text: 'ABOUT', herf: 'about' }
+        {  name: 'news', text: 'Top', herf: '/top' },
+        {  name: 'blog', text: 'News', herf: '/news' },
       ],
-      time: new Date()
     }
   },
   created () { },
   beforeMount () { },
   mounted () {
-    setInterval(this.getTimeNow, 1000)
-  },
-  computed: {
-    formatTime () {
-      return `${this.pollyTime(this.time.getHours())}:${this.pollyTime(
-        this.time.getMinutes()
-      )}:${this.pollyTime(this.time.getSeconds())}`
-    },
-    tabName () {
-      return this.$route.name
-    }
   },
   methods: {
-    getTimeNow () {
-      this.time = new Date()
-    },
-    pollyTime (t) {
-      let _t = +t < 10 ? `0${t}` : t
-      return _t
-    },
     changeTab (herf) {
       this.$router.push(herf)
     }
---
+++ src/components/imgurls.js
@@ -0,0 +1,18 @@
+import logo from '../assets/images/logo.png'
+import left from '../assets/images/left.png'
+import right from '../assets/images/right.png'
+import user from '../assets/images/user.png'
+import msg from '../assets/images/msg.png'
+import up from '../assets/images/up.png'
+
+const images = {
+  logo,
+  left,
+  right,
+  user,
+  msg,
+  up
+}
+
+
+export { images }

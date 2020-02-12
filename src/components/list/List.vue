a089ff813eb46cf8900765f9b44e5f7c560a3844
sjc <songjunchao.cn@gmail.com>
2020-02-13 05:02:04 +0800

    溢出文字处理

--- src/components/list/List.vue
+++ src/components/list/List.vue
@@ -9,26 +9,26 @@
           <div class="score">{{item.score}}</div>
         </div>
         <div class="middle">
-          <div class="title">{{item.title}}</div>
+          <div class="title">
+            {{item.title}}
+            <span class="time">{{$dateFormat(item.time,'yyyy:MM:dd hh:mm:ss') }}</span>
+          </div>
           <div class="url">{{item.url}}</div>
           <div class="comment">
             <!-- <div class="comment-wrap"> -->
-              <div>
-                <img :src="$imgUrls.images.user" alt />
-                <span class="user">44</span>
-              </div>
-              <div>
-                <img :src="$imgUrls.images.msg" alt />
-                <span class="msg">22</span>
-              </div>
+            <div>
+              <img :src="$imgUrls.images.user" alt />
+              <span class="user">{{item.by}}</span>
+            </div>
+            <div>
+              <img :src="$imgUrls.images.msg" alt />
+              <span class="msg">{{item.descendants}}</span>
+            </div>
             <!-- </div> -->
           </div>
         </div>
-        <div class="right">
-          {{$dateFormat(item.time,'yyyy:MM:dd  hh:mm:ss') }}
-        </div>
       </div>
-      <div class="line" v-if="$attrs.listData.length!==index + 1">{{$attrs.listData.length}}</div>
+      <div class="line" v-if="$attrs.listData.length!==index + 1"></div>
     </article>
   </div>
 </template>
